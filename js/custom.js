91d32b4865eaa3cb8d5288794ab56dad51783eec
Debashish <debashish4@gmail.com>
2017-04-08 23:01:00 +0530

    loader changed and logo added to preloader

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@
 // PRELOADER
 
 $(window).load(function(){
-    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+    $('#loader-wrapper').delay(1700).fadeOut("slow"); // set duration in brackets    
 });
 
 // HOME BACKGROUND SLIDESHOW
